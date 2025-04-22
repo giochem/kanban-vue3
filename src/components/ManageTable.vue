ccff1a95987165101936e2f12ae75850cd289d27
xtruong <tranxuantruong203hn@gmail.com>
2025-04-22 17:18:21 +0700

    ✨ : add edit task

--- src/components/ManageTable.vue
+++ src/components/ManageTable.vue
@@ -1,17 +1,18 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+const newTask = ref('')
 const tasks = ref([
   [
-    { title: 'task1', description: 'desc', col: 0, id: 1 },
-    { title: 'task2', description: 'desc', col: 0, id: 2 },
+    { title: 'task1', description: 'desc', col: 0, id: 1, edit: false },
+    { title: 'task2', description: 'desc', col: 0, id: 2, edit: false },
   ],
   [
-    { title: 'task3', description: 'desc', col: 1, id: 3 },
-    { title: 'task4', description: 'desc', col: 1, id: 4 },
+    { title: 'task3', description: 'desc', col: 1, id: 3, edit: false },
+    { title: 'task4', description: 'desc', col: 1, id: 4, edit: false },
   ],
   [
-    { title: 'task5', description: 'desc', col: 2, id: 5 },
-    { title: 'task6', description: 'desc', col: 2, id: 6 },
+    { title: 'task5', description: 'desc', col: 2, id: 5, edit: false },
+    { title: 'task6', description: 'desc', col: 2, id: 6, edit: false },
   ],
 ])
 
@@ -39,27 +40,58 @@ function onDragstart(ev: any) {
   ev.dataTransfer.setData('id', ev.target.id)
   ev.dataTransfer.setData('col', ev.target.getAttribute('col'))
 }
+function addTask(ev: any) {
+  console.log('add task', newTask.value)
+  tasks.value[0].push({
+    title: newTask.value,
+    description: 'desc',
+    col: 0,
+    id: tasks.value.flat().length + 1,
+    edit: false,
+  })
+
+  newTask.value = ''
+}
+function onDbclick(ev: any) {
+  console.log('onDbclick')
+}
 </script>
 <template>
-  <div class="table">
-    <div
-      class="column"
-      @drop="onDrop"
-      @dragexit.prevent
-      @dragover.prevent
-      v-for="(taskCol, index) in tasks"
-      :col="index"
-    >
+  <div class="dashboard">
+    <div class="new-task">
+      <input v-model="newTask" type="text" />
+      <button @click.prevent="addTask">Add</button>
+    </div>
+    <div class="table">
       <div
-        v-for="task in taskCol"
-        draggable="true"
-        @dragstart="onDragstart"
-        :id="String(task.id)"
-        :col="String(task.col)"
-        class="cell"
+        class="column"
+        @drop="onDrop"
+        @dragexit.prevent
+        @dragover.prevent
+        v-for="(taskCol, index) in tasks"
+        :col="index"
       >
-        <h3>{{ task.title }}</h3>
-        <p>{{ task.description }}</p>
+        <div
+          v-for="task in taskCol"
+          draggable="true"
+          @dragstart="onDragstart"
+          :id="String(task.id)"
+          :col="String(task.col)"
+          class="cell"
+        >
+          <div v-if="task.edit === false">
+            <h3 @dblclick="task.edit = true">{{ task.title }}</h3>
+            <p>{{ task.description }}</p>
+          </div>
+          <div v-else>
+            <input
+              type="text"
+              v-model="task.title"
+              @keyup.enter="task.edit = false"
+              @blur="task.edit = false"
+            />
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -80,5 +112,8 @@ function onDragstart(ev: any) {
 
 .cell {
   height: 100px;
+}
+input {
+  margin: 18.72px 0px;
 }
 </style>
